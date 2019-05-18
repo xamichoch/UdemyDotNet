0f9f8dc8c2f364341b48acfe192bd49ff173bce1
marco bergia <makobergia@gmail.com>
2019-05-18 09:16:40 +0200

    Struttura pagina corsi

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,35 +9,44 @@ using Microsoft.Extensions.Hosting;
 namespace CorsoDotnet
 {
     public class Startup
-    {
+    {        
         // Qui dentro aggiungiamo i servizi che vogliamo usare nel metodo Configure
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddMvc()
-                    .AddMvcOptions(option => {
-                        option.EnableEndpointRouting = false;
-                    });
+            services.AddMvc()
+                     .AddMvcOptions(option =>
+                     {
+                         option.EnableEndpointRouting = false;
+                     });
         }
 
+
         // Qui dentro configuriamo, aggiungendo dei middleware, la pipeline di esecuzione delle richieste alla nostra applicazione
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
         {
+            if (env.IsDevelopment())
+            {
+                lifetime.ApplicationStarted.Register(() =>
+                {
+                    string filePath = Path.Combine(env.ContentRootPath, "bin/reload.txt");
+                    File.WriteAllText(filePath, DateTime.Now.ToString());
+                });
+
+                app.UseDeveloperExceptionPage();
+            }
+
             if (env.IsProduction())
             {
                 app.UseHttpsRedirection();
             }
-            
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
             app.UseStaticFiles();
 
-            app.UseMvc(configureRoutes => {
+            app.UseMvc(configureRoutes =>
+            {
                 configureRoutes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            });          
-            
+            });
+
         }
     }
 }
